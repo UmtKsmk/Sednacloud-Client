e01d4cfbac4e82eea9435008c2e92c905bc8e447
Umut Kösemek <umt.ksmk09@gmail.com>
2024-07-09 16:27:18 +0300

    (Register - Login - UserService) creation - css fix

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterModule } from './register/register.module';
+import { LoginModule } from './login/login.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RegisterModule,
+    LoginModule,
   ]
 })
 export class ComponentsModule { }
